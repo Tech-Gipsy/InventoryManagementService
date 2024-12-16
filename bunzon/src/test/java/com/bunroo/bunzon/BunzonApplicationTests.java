a5d22c519041cd3a99c12a6aecaf0643efd529a2
Tech-Gipsy <ksairakesh@gmail.com>
2024-12-16 14:33:47 +0530

    Srping Initialization

---
+++ bunzon/src/test/java/com/bunroo/bunzon/BunzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bunroo.bunzon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BunzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
